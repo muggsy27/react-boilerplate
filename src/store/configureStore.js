30b86867fe792ae7aa24fd4766090cf392b231f6
Derek Fletes <derekfletes@gmail.com>
2018-08-20 11:29:47 -0700

    Init commit

---
+++ src/store/configureStore.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
+
+const componseEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => {
+    const store = createStore(
+        combineReducers({
+            auth: authReducer
+        }),
+        componseEnhancers(applyMiddleware(thunk))
+    );
+
+    return store;
+};
---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Header } from '../../components/Header';
+import { start } from 'repl';
+
+
+test('should render header correctly', () => {
+  const wrapper = shallow(<Header startLogout={() => { }} />);
+  expect(wrapper).toMatchSnapshot();
+
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+test('should call start logout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
+});
